e088cf8a44fe3e50ca29710fdc976b2c918bded0
François <francois.desprez@hotmail.fr>
2017-11-28 14:42:32 +0100

    ajout entité

--- Symfony/src/Hopital/AdministrationBundle/Controller/DefaultController.php
+++ Symfony/src/Hopital/AdministrationBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('HopitalAdministrationBundle:Default:index.html.twig');
+        return $this->render('HopitalAdministrationBundle:Default:base.html.twig');
     }
 }
---
+++ Symfony/src/Hopital/AdministrationBundle/Entity/Patient.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Hopital\AdministrationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Patient
+ *
+ * @ORM\Table(name="patient")
+ * @ORM\Entity(repositoryClass="Hopital\AdministrationBundle\Repository\PatientRepository")
+ */
+class Patient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numSecu", type="string", length=255)
+     */
+    private $numSecu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateNaiss", type="date")
+     */
+    private $dateNaiss;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="estAssure", type="boolean")
+     */
+    private $estAssure;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numSecu
+     *
+     * @param string $numSecu
+     *
+     * @return Patient
+     */
+    public function setNumSecu($numSecu)
+    {
+        $this->numSecu = $numSecu;
+
+        return $this;
+    }
+
+    /**
+     * Get numSecu
+     *
+     * @return string
+     */
+    public function getNumSecu()
+    {
+        return $this->numSecu;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Patient
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Patient
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set dateNaiss
+     *
+     * @param \DateTime $dateNaiss
+     *
+     * @return Patient
+     */
+    public function setDateNaiss($dateNaiss)
+    {
+        $this->dateNaiss = $dateNaiss;
+
+        return $this;
+    }
+
+    /**
+     * Get dateNaiss
+     *
+     * @return \DateTime
+     */
+    public function getDateNaiss()
+    {
+        return $this->dateNaiss;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Patient
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Patient
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set estAssure
+     *
+     * @param boolean $estAssure
+     *
+     * @return Patient
+     */
+    public function setEstAssure($estAssure)
+    {
+        $this->estAssure = $estAssure;
+
+        return $this;
+    }
+
+    /**
+     * Get estAssure
+     *
+     * @return bool
+     */
+    public function getEstAssure()
+    {
+        return $this->estAssure;
+    }
+}
+
